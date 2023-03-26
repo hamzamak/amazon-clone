314ba2d449f28c90a46308722eae3a5e2ec12095
hamzamak <makhkhas.hamza@gmail.com>
2023-03-26 15:21:34 +0000

    first-commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ function Header() {
         {/* Top nav */}
         <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
             <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-                <Image src='https://links.papareact.com/f90' width={150} height={40} style={{objectFit:"contain",width :150, height :40}} alt='logo amazon' className='cursor-pointer' onClick={()=> router.push('/')} priority />
+                <Image src='https://links.papareact.com/f90' width={150} height={40} style={{objectFit:"contain",width :150, height :40}} alt='logo amazon' className='cursor-pointer' onClick={()=> router.push('/')} />
             </div>
             {/* Search Bar  */}
             <div className='hidden sm:flex h-10 items-center rounded-md bg-yellow-400 hover:bg-yellow-500 flex-grow cursor-pointer'>
